4f9a56463be9b8674e2f016b8c1a447e1c05481d
Vikrant gupta <tayal12110@gmail.com>
2026-01-21 07:55:30 +0000

    Preview not showing

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -1,5 +1,4 @@
 import { UserNav } from '@/components/dashboard/user-nav';
-import { SidebarTrigger } from '../ui/sidebar';
 
 type DashboardHeaderProps = {
   title: string;
@@ -10,7 +9,6 @@ export function DashboardHeader({ title, children }: DashboardHeaderProps) {
   return (
     <header className="flex h-16 items-center justify-between border-b bg-background/80 px-4 backdrop-blur-sm sm:px-6">
       <div className="flex items-center gap-2">
-        <SidebarTrigger className="md:hidden" />
         <h1 className="font-headline text-xl font-semibold tracking-tight md:text-2xl">
           {title}
         </h1>
