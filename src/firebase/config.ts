3cc267ff316705895113dd21f47dd17969acacf1
Vikrant gupta <tayal12110@gmail.com>
2026-01-24 16:47:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2683820186-b0db6",
+  "appId": "1:1085165840020:web:8ac0410908b34b51f37681",
+  "apiKey": "AIzaSyAcZvfKrSDpBSjTyKjMwZk6uUk4eACoAtE",
+  "authDomain": "studio-2683820186-b0db6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1085165840020"
+};
